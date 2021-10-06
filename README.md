aa0e9eff32e811bb8627333d1b41def48a697c75
paulh-afk <paul.helleu@orange.fr>
2021-10-06 22:20:44 +0200

    new array structuring first version with horizontal conditions and vertical conditions

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Puissance 4 🟥🟩
-### 🎮 Le jeu officiel du puissance 4 fait entièrement en vanilla js avec implémentation de webpack.
+# Puissance 4 🔴🟢
+
+### 🎮 Le jeu officiel du _puissance 4_ fait entièrement en **vanilla js** avec implémentation de **webpack**.
+
+#### Prérequis pour lancer le jeu
+
+##### Pour lancer le jeu il vous faut installer toutes les dépendances et faire une installation de **webpack** globale _(npm i -D && npm i -g **webpack webpack-cli**)_ pour lancer le serveur sur votre machine en local.
+
+##### La commande **webpack serve** permet de lancer un serveur websocket qui permet d'écouter le port 8080 de votre machine
--- src/js/app.js
+++ src/js/app.js
@@ -4,56 +4,107 @@ import openModal from './modals';
 const columnElements = document.querySelectorAll('.start-piece');
 const pieceElements = document.querySelectorAll('.piece');
 
-const arrSchema = [[], [], [], [], [], [], []];
-let formatArr = [];
+let pieceArr = [];
+
+pieceElements.forEach((elem) => {
+  pieceArr.push([Number(elem.attributes.columnId.value), Number(elem.attributes.rowId.value), 0]);
+});
 const nbPieceArr = [0, 0, 0, 0, 0, 0, 0];
 // nombre de pièces dans le tableau ex: [1, 0, 4, 3]
+
+// dry functions required
+const columnsPiecesArr = () => {
+  const newArr = [[], [], [], [], [], [], []];
+  let ite = 0;
+
+  for (let i = 0; i < newArr.length * 6; i++) {
+    if (ite === newArr.length) {
+      ite = 0;
+    }
+    newArr[ite].push(pieceArr[i]);
+
+    ite++;
+  }
+  return newArr;
+};
+
+const rowsPiecesArr = () => {
+  const newArr = [[], [], [], [], [], []];
+  let r = 0;
+
+  for (let i = 0; i < newArr.length * 7; i++) {
+    if (!(i % 7)) {
+      r++;
+    }
+    newArr[r - 1].push(pieceArr[i]);
+  }
+  return newArr;
+};
 
 let playerTurn = 1;
 // 1 : vert
 // 2 : rouge
 
-// const columnsArr = formatArr.map(() => {});
-// const rowsArr = formatArr.map((colArr) => {
-//   const arr = [];
-//   arr.push();
-
-//   return arr;
-// });
-
-const addPieceArr = (column) => {
-  nbPieceArr[column] += 1;
-};
-
 const winCondition = () => {
   let winConditionVertical = 0;
   let winConditionHorizontal = 0;
 
-  formatArr.map((columnsArr) => {
-    columnsArr.map((arr) => {
-      // vertical condition
+  columnsPiecesArr().map((columnArr) => {
+    columnArr.map((arr) => {
       if (arr[2] === playerTurn) {
         winConditionVertical += 1;
         if (winConditionVertical === 4) {
           openModal(
             `Le joueur ${
               playerTurn === 1 ? 'Rouge' : 'Vert'
-            } gagne la partie avec un alignement vertical !`
+            } gagne la partie avec un alignement vertical !`,
           );
         }
       } else {
         winConditionVertical = 0;
       }
-
-      // horizontal condition
-      // [[1, 6, 2]] [[2, 6, 2]]
-
-      // console.log(arr[2]);
     });
-    if (columnsArr[0]) {
-
-    }
   });
+
+  rowsPiecesArr().map((rowArr) => {
+    rowArr.map((arr) => {
+      if (arr[2] === playerTurn) {
+        winConditionHorizontal += 1;
+        if (winConditionHorizontal === 4) {
+          openModal(
+            `Le joueur ${
+              playerTurn === 1 ? 'Rouge' : 'Vert'
+            } gagne la partie avec un alignement horizontal !`,
+          );
+        }
+      } else {
+        winConditionHorizontal = 0;
+      }
+    });
+  });
+
+  // formatArr.map((columnsArr) => {
+  //   columnsArr.map((arr) => {
+  //     // vertical condition
+  //     if (arr[2] === playerTurn) {
+  //       winConditionVertical += 1;
+  //       if (winConditionVertical === 4) {
+  //         openModal(
+  //           `Le joueur ${
+  //             playerTurn === 1 ? 'Rouge' : 'Vert'
+  //           } gagne la partie avec un alignement vertical !`,
+  //         );
+  //       }
+  //     } else {
+  //       winConditionVertical = 0;
+  //     }
+
+  //     // horizontal condition
+  //     // [[1, 6, 2]] [[2, 6, 2]]
+
+  //     // console.log(arr[2]);
+  //   });
+  // });
 
   // comparaison du tableau
   // diagonale => vérification de l'autre column avec l'index supérieur ou inférieur
@@ -61,13 +112,13 @@ const winCondition = () => {
 };
 
 const changeCSSClass = () => {
-  columnElements.forEach((element) => {
+  columnElements.forEach((column) => {
     if (playerTurn === 2) {
-      element.classList.add('red-turn');
-      element.classList.remove('green-turn');
+      column.classList.add('red-turn');
+      column.classList.remove('green-turn');
     } else {
-      element.classList.add('green-turn');
-      element.classList.remove('red-turn');
+      column.classList.add('green-turn');
+      column.classList.remove('red-turn');
     }
   });
 };
@@ -95,14 +146,16 @@ const changePlayerTurn = () => {
 const addPiece = (column) => {
   if (nbPieceArr[column - 1] === 6) {
     openModal('Vous ne pouvez pas ajouter de pièce ici !');
-    // openmodal de réinitialisation du jeu ou annuler le coup
+    return null;
+    // openmodal de réinitialisation du jeu ou annuler le coup, second paramètre contient le texte à afficher dans les boutons
   }
+  nbPieceArr[column - 1] += 1;
 
-  formatArr[column - 1][nbPieceArr[column - 1]][2] = playerTurn;
-  const row = formatArr[column - 1][nbPieceArr[column - 1]][1];
+  console.log(columnsPiecesArr()[column - 1][nbPieceArr[column - 1] - 1]);
+  columnsPiecesArr()[column - 1][nbPieceArr[column - 1] - 1][2] = playerTurn;
+  const row = nbPieceArr[column - 1];
 
   changeCSSClass();
-  addPieceArr(column - 1);
   winCondition();
   displayPiece(column, row);
   changePlayerTurn();
@@ -115,18 +168,3 @@ columnElements.forEach((column) => {
     addPiece(Number(value));
   });
 });
-
-const format = (arr) => {
-  let row = 6;
-
-  for (let i = 0; i < 7; i += 1) {
-    for (let e = 0; e < 6; e += 1) {
-      arr[i].push([i + 1, row, 0]);
-      row -= 1;
-    }
-    row = 6;
-  }
-  return arr;
-};
-
-formatArr = format(arrSchema);
