43484cb91f62f1fd7d89cb4b510558966507ca4a
PauL <57185255+paulh-afk@users.noreply.github.com>
2022-08-14 16:17:55 +0200

    animated favicon

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,8 @@
 import '../style.css';
+import faviconImg from '../img/connect4-icon.gif';
+
+// Include animated favicon
+const favicon = document.createElement('link');
+favicon.rel = 'icon';
+favicon.href = faviconImg;
+document.head.appendChild(favicon);
